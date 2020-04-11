01b804bf27a8d17815474569fc906bf7bca7f69c
RaulAbejonDelgado <raul.abejon.delgado@gmail.com>
2020-04-11 14:35:10 +0200

    Custom source

---
+++ custom-source/src/test/java/com/raul/custom/source/customsource/CustomSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raul.custom.source.customsource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomSourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
