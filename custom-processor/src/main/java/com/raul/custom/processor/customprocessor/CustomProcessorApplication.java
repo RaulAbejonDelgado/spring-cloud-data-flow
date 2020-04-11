ebc23d09c4bee4a9b55c57fa4c765c5939d59eec
RaulAbejonDelgado <raul.abejon.delgado@gmail.com>
2020-04-11 13:23:05 +0200

    Custom processor

---
+++ custom-processor/src/main/java/com/raul/custom/processor/customprocessor/CustomProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.raul.custom.processor.customprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomProcessorApplication.class, args);
+	}
+
+}
---
+++ custom-processor/src/test/java/com/raul/custom/processor/customprocessor/CustomProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raul.custom.processor.customprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
